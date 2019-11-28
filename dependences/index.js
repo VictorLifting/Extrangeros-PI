efcbeaf069dd3e66ea3d44123cdf9e18e901068e
Nicolas Medina P <nickmedina99@hotmail.com>
2019-11-28 13:06:37 -0500

    pnatalla login espanol y registro espanol

--- dependences/index.js
+++ dependences/index.js
@@ -7,6 +7,17 @@ botones[1].addEventListener("click", function () {
     contenido.classList.add("slideOutUp")
 
     let retrasoCambio = setTimeout(function () {
+        document.location.href = "./login.html"
+    }, 1000)
+
+})
+
+botones[0].addEventListener("click", function () {
+
+
+    contenido.classList.add("slideOutUp")
+
+    let retrasoCambio = setTimeout(function () {
         document.location.href = "./pantalla1.html"
     }, 1000)
 
