aa166d3ca01b191bb9b5730b5534cb937cf4943d
Victor Espejo <contactovictore@gmail.com>
2019-12-01 20:04:51 -0500

    agregue pantallas

---
+++ dependences/pantalla2.js
@@ -0,0 +1,64 @@
+let botones = document.querySelectorAll(".go")
+let contenido = document.querySelector(".contenido")
+let atras = document.querySelector(".atras")
+
+botones[0].addEventListener("click", function () {
+
+
+   // contenido.classList.add("slideOutUp")
+
+    let retrasoCambio = setTimeout(function () {
+        document.location.href = "./thecontext2.html"
+    }, 1000)
+
+})
+botones[1].addEventListener("click", function () {
+
+
+    // contenido.classList.add("slideOutUp")
+ 
+     let retrasoCambio = setTimeout(function () {
+         document.location.href = "./theculture1.html"
+     }, 1000)
+ 
+ })
+ botones[2].addEventListener("click", function () {
+
+
+    // contenido.classList.add("slideOutUp")
+ 
+     let retrasoCambio = setTimeout(function () {
+         document.location.href = "./thetime1.html"
+     }, 1000)
+ 
+ })
+ botones[3].addEventListener("click", function () {
+
+
+    // contenido.classList.add("slideOutUp")
+ 
+     let retrasoCambio = setTimeout(function () {
+         document.location.href = "./thedetails.html"
+     }, 1000)
+ 
+ })
+ botones[4].addEventListener("click", function () {
+
+
+    // contenido.classList.add("slideOutUp")
+ 
+     let retrasoCambio = setTimeout(function () {
+         document.location.href = "./moreinfo.html"
+     }, 1000)
+ 
+ })
+ atras.addEventListener("click", function () {
+
+
+    // contenido.classList.add("slideOutUp")
+ 
+     let retrasoCambio = setTimeout(function () {
+         document.location.href = "./choose.html"
+     }, 1000)
+ 
+ })
